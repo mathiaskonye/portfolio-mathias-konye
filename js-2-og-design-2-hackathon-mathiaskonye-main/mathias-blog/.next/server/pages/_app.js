265ef35a234a2b7c205a6641181d11f3ac5929a0
mathiaskonye <56630416+mathiaskonye@users.noreply.github.com>
2021-05-27 00:44:07 +0200

    update

---
+++ js-2-og-design-2-hackathon-mathiaskonye-main/mathias-blog/.next/server/pages/_app.js
@@ -0,0 +1,65 @@
+(function() {
+var exports = {};
+exports.id = 888;
+exports.ids = [888];
+exports.modules = {
+
+/***/ 707:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": function() { return /* binding */ App; }
+});
+
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__(9297);
+var external_react_default = /*#__PURE__*/__webpack_require__.n(external_react_);
+;// CONCATENATED MODULE: external "@fortawesome/fontawesome-free/js/fontawesome"
+var fontawesome_namespaceObject = require("@fortawesome/fontawesome-free/js/fontawesome");;
+;// CONCATENATED MODULE: external "@fortawesome/fontawesome-free/js/solid"
+var solid_namespaceObject = require("@fortawesome/fontawesome-free/js/solid");;
+;// CONCATENATED MODULE: external "@fortawesome/fontawesome-free/js/regular"
+var regular_namespaceObject = require("@fortawesome/fontawesome-free/js/regular");;
+;// CONCATENATED MODULE: external "@fortawesome/fontawesome-free/js/brands"
+var brands_namespaceObject = require("@fortawesome/fontawesome-free/js/brands");;
+;// CONCATENATED MODULE: ./pages/_app.js
+
+var __jsx = (external_react_default()).createElement;
+
+
+
+
+
+function App({
+  Component,
+  pageProps
+}) {
+  return __jsx(Component, pageProps);
+}
+
+/***/ }),
+
+/***/ 9297:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("react");;
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+var __webpack_exports__ = (__webpack_exec__(707));
+module.exports = __webpack_exports__;
+
+})();
